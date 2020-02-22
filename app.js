ee8ed761ef8c58a54df9258e04f479c681cc421c
odortega <odortega@gmail.com>
2020-02-21 21:06:22 -0500

    first node express app response

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = new express();
+
+app.get('/',home);
+
+function home(req, res){
+    res.send('hello nodejs express');
+}
+
+app.listen(3000, writeConsole);
+
+function writeConsole(){
+    console.log('init express app');
+}
