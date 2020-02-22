5a735887fe40632c476f1aa5fe0322e6588ee382
odortega <odortega@gmail.com>
2020-02-21 21:22:58 -0500

    implement sign In

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var express = require("express");
 var app = new express();
 
 app.get('/',home);
+app.get('/signin/:city',signIn);
 
 function home(req, res){
     res.send('hello nodejs express');
+}
+
+function signIn(req,res){
+    var city = req.params.city;
+    res.send('Wellcome to ' + city + ' appeals');
 }
 
 app.listen(3000, writeConsole);
